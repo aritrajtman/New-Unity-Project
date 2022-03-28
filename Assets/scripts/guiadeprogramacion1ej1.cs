64bd0e3527f3e00a40bffaacac478e293de35cec
aritrajtman <47066506@est.ort.edu.ar>
2022-03-28 11:29:04 -0300

    Initial commit

---
+++ Assets/scripts/guiadeprogramacion1ej1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Realizá un programa donde se cree una variable entera llamada num1, que inicialmente
+//valdrá 0. Luego incrementá su valor en 2 y mostralo por pantalla.Después mostrá el
+//resultado de multiplicarlo por sí mismo.
+public class guiadeprogramacion1ej1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int num1 = 0;
+        num1 += 2;
+        Debug.Log(num1);
+        num1 = num1 * num1;
+        Debug.Log(num1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
